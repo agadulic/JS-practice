a0f596bef0617550c088b51ae065d87ce2eb7174
AgaDulic <mrs.agnieszka.michalak@gmail.com>
2017-06-03 12:22:20 +0200

    my first wish list

---
+++ script.js
@@ -0,0 +1,38 @@
+var wishes = [];
+
+function addToList(item) {
+  $('ol#items').append('<li>' + item + '<span class="label pending">Pending</span></li>');
+}
+
+function updateTotal() {
+  completed = $('.success').length;
+  pending = $('.pending').length;
+
+  if (completed > 0 || pending > 0) {
+    $('.total').text(" Pending: " + pending + " Completed: " + completed);
+  }
+}
+
+$(document).ready(function(){
+  wishes.forEach(function(element) {
+    addToList(element);
+  });
+  updateTotal();
+
+  $(document).on('click','#add-to-list',function(){
+    item = $('#item').val();
+
+    $('#item').val('');
+    $('#item').focus();
+
+    addToList(item);
+    updateTotal();
+  });
+
+  $(document).on('click','.pending',function(){
+    $(this).parent().append('<span class="label success">Done!</span>');
+    $(this).parent().attr('class', 'completed');
+    $(this).remove();
+    updateTotal();
+  });
+});
